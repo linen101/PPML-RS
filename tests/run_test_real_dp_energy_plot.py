ccf52ab22c5dbb34dae11d239b3bb213aaac360e
linen101 <linen@protonmail.com>
2025-08-26 21:39:09 +0300

    real data

--- tests/run_test_real_dp_energy_plot.py
+++ tests/run_test_real_dp_energy_plot.py
@@ -68,7 +68,7 @@ def run(X_train, Y_train, X_test, Y_test, beta):
     w_torrent, _ = torrent_admm_fxp_analyze_gauss(
         X_parts_fxp, y_parts_fxp, beta=beta,
         epsilon=0.1, rho=1, admm_steps=5, rounds=5,
-        wstar=None, dp_X=dp_X, dp_y=dp_Y
+        wstar=None, dp_noise_x=dp_X, dp_noise_y=dp_Y
     )
     
     #w_torrent, _ = torrent(X_train, Y_cor, beta, epsilon=0.1, max_iters=5)
