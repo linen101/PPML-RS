6120119eb313b5c8114cf733725a37f13486b57f
linen101 <linen@protonmail.com>
2025-08-27 23:03:28 +0300

    fxp

--- tests_fxp/run_tests_strategic_fxp.py
+++ tests_fxp/run_tests_strategic_fxp.py
@@ -91,12 +91,14 @@ def run_tests_fxp_d(n, num_trials=10):
             X_parts_fxp, y_parts_fxp = split_matrix_fxp(X_parts, y_parts)
             w_star = fxp(w_star)
             w_torrent_fxp, _= torrent_admm_fxp(X_parts_fxp, y_parts_fxp, beta, epsilon, rho, admm_steps, robust_rounds, wstar=None, dp_w=dp_w_val)
-            w_errors_d_torrent_fxp[i] += fxp(np.linalg.norm(w_torrent_fxp - w_star)) * norm_w_inv
-            print(f' sum trials error DP OLS: {w_errors_d_torrent_fxp[i].info()}')
+            error_torrent = fxp(np.linalg.norm(w_torrent_fxp - w_star)) * norm_w_inv
+            w_errors_d_torrent_fxp[i] += error_torrent
+            print(f' sum trials error DP OLS: {error_torrent.info()}')
             
             w_torrent, _ = torrent_admm_fxp_analyze_gauss(X_parts_fxp, y_parts_fxp, beta, epsilon, rho, admm_steps, robust_rounds, wstar=None, dp_noise_x=dp_noise_x_val, dp_noise_y=dp_noise_y_val)
-            w_errors_d_torrent[i] += fxp(np.linalg.norm(w_torrent - w_star)) * norm_w_inv
-            print(f' sum trials error Analyze Gauss: {w_errors_d_torrent[i].info()}')
+            error_gauss =fxp(np.linalg.norm(w_torrent - w_star)) * norm_w_inv
+            w_errors_d_torrent[i] += error_gauss
+            print(f' sum trials error Analyze Gauss: {error_gauss.info()}')
         
     # Compute averages
     w_errors_d_torrent /= num_trials
