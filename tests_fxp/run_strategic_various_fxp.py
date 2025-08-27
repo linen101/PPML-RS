3edc9abd80aeed9301426693dee2ca95747289f8
linen101 <linen@protonmail.com>
2025-08-27 14:51:09 +0300

    dp str

--- tests_fxp/run_strategic_various_fxp.py
+++ tests_fxp/run_strategic_various_fxp.py
@@ -75,7 +75,7 @@ def run_experiment(
             # Normalized error
             norm_w = np.linalg.norm((w_star))
             norm_w_inv = 1 / norm_w
-            error = np.linalg.norm(w_torrent - w_star) * w_norm
+            error = np.linalg.norm(w_torrent - w_star) * norm_w_inv
             print(f"[{mode}] {x=} dp_w={dp_w} trial {trial+1}: error={error}")
             error_accum += error
 
