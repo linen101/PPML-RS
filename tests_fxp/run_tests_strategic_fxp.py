9d92c6c506a1f16c07c982d32dba7e5f809372e3
linen101 <linen@protonmail.com>
2025-08-31 14:57:45 +0300

    fxp

--- tests_fxp/run_tests_strategic_fxp.py
+++ tests_fxp/run_tests_strategic_fxp.py
@@ -183,7 +183,7 @@ def run_tests_fxp_alpha(num_trials=10):
                 admm_steps, robust_rounds, wstar=None,
                 dp_noise_x=dp_noise_x, dp_noise_y=dp_noise_y
             )
-            errors_gauss = (np.linalg.norm(w_torrent.get_val() - w_star)) * norm_w_inv
+            error_gauss = (np.linalg.norm(w_torrent.get_val() - w_star)) * norm_w_inv
             errors_gauss[trial, i] = (error_gauss)
             print("Error AG: ", error_gauss)
             # --- Torrent DP fxp ---
@@ -191,7 +191,7 @@ def run_tests_fxp_alpha(num_trials=10):
                 X_parts_fxp, y_parts_fxp, beta, epsilon, rho,
                 admm_steps, robust_rounds, wstar=None, dp_w=dp_w
             )
-            errors_dp = (np.linalg.norm(w_torrent_fxp.get_val() - w_star)) * norm_w_inv
+            error_dp = (np.linalg.norm(w_torrent_fxp.get_val() - w_star)) * norm_w_inv
             errors_dp[trial, j] = error_dp
             print("Error DP: ", error_dp)
     # Compute mean and variance
